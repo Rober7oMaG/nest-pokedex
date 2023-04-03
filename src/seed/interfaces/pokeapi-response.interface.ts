82fe40ba576cad5549c4c81223a5c81485c189bd
Roberto Macias <robertomag7@hotmail.com>
2023-04-03 12:36:48 -0700

    Before bulk creating with seed

---
+++ src/seed/interfaces/pokeapi-response.interface.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+export interface PokeAPIResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+}
+
+export interface Result {
+  name: string;
+  url: string;
+};
